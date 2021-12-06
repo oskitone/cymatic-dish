5714c4b03fef90660171889e8e91fe4a0f3d97de
Tommy <tommy@iamnotagoodartist.com>
2021-12-06 14:55:10 -0800

    Frequency control

--- arduino/cymatic_dish/cymatic_dish.ino
+++ arduino/cymatic_dish/cymatic_dish.ino
@@ -1,16 +1,46 @@
 const int SPEAKER_PIN = 11;
+const int FREQUENCY_CONTROL_PIN = A0;
 
-const float frequency = 220;
+const int MIN = 32.97; // C6
+const int MAX = 2109.89; // C0
+
+const long THROTTLE = 10;
+
+const bool DEBUG = true;
+
+float frequency = 0;
+
+float getVoltage(int pin) {
+  return analogRead(pin) * (5.0 / 1023.0) / 5;
+}
+
+unsigned long oldMillis = 0;
+float getFrequency(bool skipPoll = false) {
+  unsigned long newMillis = millis();
+  bool pollPasses = (unsigned long)(newMillis - oldMillis) >= THROTTLE;
+
+  if (skipPoll || pollPasses) {
+    oldMillis = newMillis;
+    return round(MIN + (MAX - MIN) * getVoltage(FREQUENCY_CONTROL_PIN));
+  }
+
+  return frequency;
+}
 
 void setup() {
+  Serial.begin(9600);
   pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
 }
 
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);
-  tone(SPEAKER_PIN, frequency);
-  delay(1000);
-  digitalWrite(LED_BUILTIN, LOW);
-  noTone(SPEAKER_PIN);
-  delay(1000);
+  frequency = getFrequency();
+
+  if (DEBUG) {
+    Serial.println(frequency);
+  }
+
+  if (frequency > 0) {
+    tone(SPEAKER_PIN, frequency);
+  }
 }
